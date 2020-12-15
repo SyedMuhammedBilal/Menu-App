e63e9680fc5926f155bbb689d18e7572f67e4367
Syed Muhammed Bilal <intergo878@gmail.com>
2020-12-15 22:03:17 +0500

    new commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Menu from './components/Menu'
+import Categories from './components/Categories'
+import Item from './data'
 
 function App() {
+
+  const [menuItems, setMenuItems] = useState(Item);
+  const [categories, setCategories] = useState([]);
+
   return (
-    <div className="App">
+    <main>
+      <section className='menu section'>
+        <div className='title'>
+          <h2>Our Menu</h2>
+          <div className='underline'></div>
+        </div>
+        <Categories />
+        <Menu />
+      </section>
       
-    </div>
+    </main>
   );
 }
 
